bf95b2d07eedcef56682d61786974c0c0b102c2f
Alex Nazarov <alexx.nazaroff@gmail.com>
2024-10-17 16:06:58 +0300

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "1.32.0"
+
+let gitUrl = "https://raw.githubusercontent.com/SumSubstance/IdensicMobileSDK-iOS-Release/master/\(version)"
+
+enum checksums {
+
+    static let msdk = "73f5c783c65e3a3f1464807a1bce3b39ada2a1be6a271d025c081bf62848540f"
+
+}
+
+let package = Package(
+    name: "IdensicMobileSDK",
+    platforms: [
+        .iOS("12.2")
+    ],
+    products: [
+        .library(
+            name: "IdensicMobileSDK",
+            targets: [
+                "IdensicMobileSDK"
+            ]
+        )
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "IdensicMobileSDK",
+            url: "\(gitUrl)/IdensicMobileSDK_Core-\(version).zip",
+            checksum: checksums.msdk
+        )
+    ]
+)
