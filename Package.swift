42af3056bee6000c70ebcd01d126ffdbdccafdb0
Roman Silin <roman.s@sumsub.com>
2025-11-13 16:18:28 +0300

    Release 1.39.1

--- Package.swift
+++ Package.swift
@@ -3,13 +3,13 @@
 
 import PackageDescription
 
-let version = "1.39.0"
+let version = "1.39.1"
 
 let gitUrl = "https://raw.githubusercontent.com/SumSubstance/IdensicMobileSDK-iOS-Release/master/\(version)"
 
 enum checksums {
 
-    static let msdk = "ab16897921f8f5ff67942496c5d61a97afff2038dc80a2c7a6a34708cb8b11a8"
+    static let msdk = "e9aea1c55278e99c1fb9751f9d61292a0fa5cf4c5c639dfa90ae1484e69d1299"
 
 }
 
