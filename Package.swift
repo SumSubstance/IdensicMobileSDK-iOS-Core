4bae2ee1ba5bd8c949405caf753bdc501f9cacc0
Alexey Nazarov <alexey.nazarov@ptr.vc>
2025-11-27 20:10:04 +0200

    Release 1.40.0

--- Package.swift
+++ Package.swift
@@ -3,20 +3,18 @@
 
 import PackageDescription
 
-let version = "1.39.1"
+let version = Version("1.40.0")
 
 let gitUrl = "https://raw.githubusercontent.com/SumSubstance/IdensicMobileSDK-iOS-Release/master/\(version)"
 
 enum checksums {
-
-    static let msdk = "e9aea1c55278e99c1fb9751f9d61292a0fa5cf4c5c639dfa90ae1484e69d1299"
-
+    static let msdk = "8aa38d2e1667a76efb28b02efca766aab004eaf9830ce8dbc7d0a4046617c6fe"
 }
 
 let package = Package(
     name: "IdensicMobileSDK",
     platforms: [
-        .iOS("12.2")
+        .iOS("13.0")
     ],
     products: [
         .library(
